5b0f74b333df543b8269a4603224bef2121e93cb
Tail_Straw <maeva.gr@live.fr>
2024-10-29 23:04:16 +0100

    Notion-15 Config multer

--- app.js
+++ app.js
@@ -1,28 +1,23 @@
 const express = require('express');
 const cors = require('cors');
-const dotenv = require('dotenv').config();
 const bodyParser = require('body-parser');
+const path = require('path');
+const dotenv = require('dotenv').config();
 const mongoose = require('mongoose');
 
 const app = express();
+
+const gameRoutes = require('./routes/game');
 
 mongoose.connect(process.env.MONGODB_URL)
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !')
 );
 
-app.use(cors());
+app.use(cors('*'));
 app.use(bodyParser.json());
 
-// app.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-//     next();
-// });
-
-app.get('/', (req, res) => {
-    res.json({ message: "Hello from server!" });
-});
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/api/game', gameRoutes);
 
 module.exports = app;
